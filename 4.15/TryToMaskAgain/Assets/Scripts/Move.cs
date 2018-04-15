4339edb525abc53bf573d8aebd638751c0f746fb
kongooo <1184701824@qq.com>
2018-04-15 15:50:37 +0800

    try

--- 4.15/TryToMaskAgain/Assets/Scripts/Move.cs
+++ 4.15/TryToMaskAgain/Assets/Scripts/Move.cs
@@ -4,27 +4,11 @@ using UnityEngine;
 
 public class Move : MonoBehaviour {
 
-    private bool Stop = false;
     private Vector3 direction = Vector3.zero;
     public float speed = 2.0f;
 
 	void Update () {
-        move();
-	}
-
-    void move()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-            direction = new Vector3(-1, 0, 0);
-        if (Input.GetKeyDown(KeyCode.D))
-            direction = new Vector3(1, 0, 0);
-        if (Input.GetKeyDown(KeyCode.W))
-            direction = new Vector3(0, 0, 1);
-        if (Input.GetKeyDown(KeyCode.S))
-            direction = new Vector3(0, 0, -1);
-        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
-            direction = Vector3.zero;
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         GetComponent<Rigidbody>().MovePosition(transform.position + direction * Time.deltaTime * speed);
     }
-    
 }
